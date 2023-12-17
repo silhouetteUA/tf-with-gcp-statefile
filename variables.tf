cd5674807a392391661d1bbd7671d58a66cf5302
Yevhenii Dmytriiev <dmytriiev.yevhenii@gmail.com>
2023-12-17 14:05:31 -0500

    local deployment is finished

--- variables.tf
+++ variables.tf
@@ -1,13 +1,36 @@
-variable "GOOGLE_REGION" {
+# variable "GOOGLE_REGION" {
+#   type    = string
+#   default = "us-east1"
+# }
+
+# variable "GOOGLE_PROJECT" {
+#   type = string
+# }
+
+# variable "GKE_NUM_NODES" {
+#   type    = number
+#   default = 1
+# }
+
+# LOCAL deployment STARTS HERE (localhost KIND cluster + FluxCD + repo reconciliation)
+
+variable "local_kind_cluster_name" {
   type    = string
-  default = "us-east1"
+  default = "test-cluster"
 }
 
-variable "GOOGLE_PROJECT" {
-  type = string
+# export TF_VAR_github_token=XXXXXXX   OR:   TF_VAR_my_variable="XXXXX" terraform apply
+variable "github_token" {
+  sensitive = true
+  type      = string
 }
 
-variable "GKE_NUM_NODES" {
-  type    = number
-  default = 1
+variable "github_org" {
+  type    = string
+  default = "silhouetteUA"
+}
+
+variable "github_repository" {
+  type    = string
+  default = "kbot"
 }
